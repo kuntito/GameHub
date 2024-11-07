bcf1876ff7f3dd2a28bb273706769c71f0effee9
kuntito <owolabitito@gmail.com>
2024-11-07 22:49:31 +0000

    built a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { defaultSystem } from "@chakra-ui/react"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
         <App />
     </ChakraProvider>
   </React.StrictMode>,
