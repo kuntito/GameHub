499ec1e91ed7cb22d2e02fdbfb4ce89133176c26
kuntito <owolabitito@gmail.com>
2024-11-18 16:15:14 +0000

    clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,8 @@ import {
   Text,
   Spinner,
   Button,
+  Box,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
@@ -24,27 +26,33 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={ genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-              fontSize="lg"
-              variant="link"
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <Box paddingX={3}>
+        <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+        <List>
+        {data.map((genre) => (
+            <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+                <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit='cover'
+                src={getCroppedImageUrl(genre.image_background)}
+                />
+                <Button
+                onClick={() => onSelectGenre(genre)}
+                fontWeight={ genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+                fontSize="lg"
+                variant="link"
+                whiteSpace='normal'
+                textAlign='left'
+                >
+                {genre.name}
+                </Button>
+            </HStack>
+            </ListItem>
+        ))}
+        </List>
+    </Box>
   );
 };
 
